fec5f16710b489639261996d74d5bf6b2f013b1c
shulgan@email.ua <shulgan@email.ua>
2017-10-25 23:38:38 +0300

    update ExcelWriter

---
+++ ConsoleAppTestFinaly/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelWriter;
+using ShopWorkWithDB;
+
+
+namespace ConsoleAppTestFinaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Discount> discs = new List<Discount>();
+            discs.Add(new Discount() { Description = "DiscountDescription_1", Id = 1, Percent = 10 });
+            discs.Add(new Discount() { Description = "DiscountDescription_2", Id = 2, Percent = 20 });
+            discs.Add(new Discount() { Description = "DiscountDescription_3", Id = 3, Percent = 30 });
+            discs.Add(new Discount() { Description = "DiscountDescription_4", Id = 4, Percent = 40 });
+
+            List<ProductType> prodtypes = new List<ProductType>();
+            prodtypes.Add(new ProductType() { Id = 1, Name = "ProductType_1" });
+            prodtypes.Add(new ProductType() { Id = 2, Name = "ProductType_2" });
+            prodtypes.Add(new ProductType() { Id = 3, Name = "ProductType_3" });
+            prodtypes.Add(new ProductType() { Id = 4, Name = "ProductType_4" });
+
+            List<Product> p = new List<Product>();
+            p.Add(new Product() { Id = 1, Count = 1, Name = "product1", Discount = discs[0], Price = 100, Type = prodtypes[3] });
+            p.Add(new Product() { Id = 2, Count = 2, Name = "product2", Discount = discs[1], Price = 200, Type = prodtypes[2] });
+            p.Add(new Product() { Id = 3, Count = 3, Name = "product3", Discount = discs[2], Price = 300, Type = prodtypes[1] });
+            p.Add(new Product() { Id = 4, Count = 4, Name = "product4", Discount = discs[3], Price = 400, Type = prodtypes[0] });
+            p.Add(new Product() { Id = 5, Count = 5, Name = "product5", Discount = discs[0], Price = 500, Type = prodtypes[3] });
+
+
+            List<Sale> s = new List<Sale>();
+            s.Add(new Sale() { Id = 1, SaleDate = DateTime.Now, SaledProducts = p.GetRange(1, 4), Sum = 000 });
+            s.Add(new Sale() { Id = 2, SaleDate = DateTime.Now, SaledProducts = p.GetRange(1, 3), Sum = 999 });
+            s.Add(new Sale() { Id = 3, SaleDate = DateTime.Now, SaledProducts = p.GetRange(3, 2), Sum = 888 });
+            s.Add(new Sale() { Id = 4, SaleDate = DateTime.Now, SaledProducts = p.GetRange(1, 1), Sum = 777 });
+
+
+            ExcelWriter.ExcelWriter.WriteToExcel(s, @"test.xls");
+        }
+
+    }
+}
---
+++ ExcelWriter/ExcelWriter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopWorkWithDB;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace ExcelWriter
+{
+    public class ExcelWriter
+    {
+        public static List<string> tableHeaders = new List<string>() {
+            "Id","SaleDate","Sum","Count","Id","Name","Price","DiscountId","DiscountDescription","DiscountPercent","TypeId","TypeName"
+        };
+
+        static public void WriteToExcel(List<Sale> sales, string path)
+        {
+            Excel.Application xlApp = new Excel.Application();
+
+            if (xlApp == null)
+            {
+                new Exception("Excel is not property installed");
+            }
+
+
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            for (int i = 0; i < tableHeaders.Count; i++)
+            {
+                xlWorkSheet.Cells[1, i + 1] = tableHeaders[i];
+            }
+
+
+            Excel.Range oRange;
+            int pos = 2;
+            int countProds;
+            for (int i = 0; i < sales.Count; i++)
+            {
+                xlWorkSheet.Cells[pos, 1] = sales[i].Id;
+                xlWorkSheet.Cells[pos, 2] = sales[i].SaleDate.ToString("u");
+                xlWorkSheet.Cells[pos, 3] = sales[i].Sum;
+                xlWorkSheet.Cells[pos, 4] = sales[i].SaledProducts.Count;
+
+                for (int j = 0; j < sales[i].SaledProducts.Count; j++)
+                {
+                    xlWorkSheet.Cells[pos, 5] = sales[i].SaledProducts[j].Id;
+                    xlWorkSheet.Cells[pos, 6] = sales[i].SaledProducts[j].Name;
+                    xlWorkSheet.Cells[pos, 7] = sales[i].SaledProducts[j].Price;
+                    xlWorkSheet.Cells[pos, 8] = sales[i].SaledProducts[j].Discount.Id;
+                    xlWorkSheet.Cells[pos, 9] = sales[i].SaledProducts[j].Discount.Description;
+                    xlWorkSheet.Cells[pos, 10] = sales[i].SaledProducts[j].Discount.Percent;
+                    xlWorkSheet.Cells[pos, 11] = sales[i].SaledProducts[j].Type.Id;
+                    xlWorkSheet.Cells[pos, 12] = sales[i].SaledProducts[j].Type.Name;
+                    ++pos;
+                }
+                countProds = sales[i].SaledProducts.Count;
+
+                for (int g = 1; g <= 4; g++)
+                {
+                    oRange = xlWorkSheet.Range[xlWorkSheet.Cells[pos - countProds, g], xlWorkSheet.Cells[pos - 1, g]];
+
+                    (xlWorkSheet.Cells[pos - countProds, g] as Excel.Range).HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                    (xlWorkSheet.Cells[pos - countProds, g] as Excel.Range).VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+
+                    oRange.Merge(Type.Missing);
+                }
+
+
+                ++pos;
+            }
+
+
+
+
+            xlWorkBook.SaveAs(path, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+        }
+    }
+}
