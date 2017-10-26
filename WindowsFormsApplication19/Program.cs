5f47f1a435ab3efdb8e10e8cefb0b58b58dca384
Semanyk Mariya <semanykmaria@gmail.com>
2017-10-26 22:25:08 +0300

    print

--- WindowsFormsApplication19/Program.cs
+++ WindowsFormsApplication19/Program.cs
@@ -17,7 +17,7 @@ namespace WindowsFormsApplication19
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Ticket());
             
 
         }
--- WindowsFormsApplication19/Ticket.cs
+++ WindowsFormsApplication19/Ticket.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,38 @@ namespace WindowsFormsApplication19
 {
     public partial class Ticket : Form
     {
+        string path;
+
+        public void PrintCheck() {
+           
+            StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8);
+            sw.WriteLine("          \"SportsEat\"");
+            sw.WriteLine("   Магазин спотивних добавок");
+            sw.WriteLine();
+            sw.WriteLine();
+            sw.WriteLine(String.Format("  Час:   {0} ", hour.Text));
+            sw.WriteLine(String.Format("  Касир:   {0} ", cashiersName.Text));
+            sw.WriteLine(String.Format("  Рахунок №  {0} ", score.Text));
+            sw.WriteLine("    ******************************    ");
+            sw.WriteLine(String.Format("  Дата:   {0} ", day.Text));
+            sw.WriteLine(String.Format("   {0}                {1}", ProductsName.Text, numbers.Text));
+            sw.WriteLine(String.Format("   {0}                {1}", nimber.Text, priceforOneProducts.Text));
+            sw.WriteLine("    ******************************    ");
+            sw.WriteLine();
+            sw.WriteLine(String.Format("  Загальна сума: {0} ", TotalPrice.Text));
+            sw.WriteLine();
+            sw.WriteLine("           м.Львів, вул.Пекарська, 10    ");
+            sw.WriteLine("              +3 (022) 296-54-85    ");
+
+            sw.Close();
+        }
+
+
         public Ticket()
         {
             InitializeComponent();
+            path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            path += "/bill.txt";
         }
 
         private void Sale_Click(object sender, EventArgs e)
@@ -37,6 +67,10 @@ namespace WindowsFormsApplication19
         private void Print_Click(object sender, EventArgs e)
         {
 
+            PrintCheck();
+            MessageBox.Show("check directory:   "+ path);
+
         }
     }
+
 }
